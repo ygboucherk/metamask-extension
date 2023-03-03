3f41f69c769477adb469ce08279b48020cfafbb4
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2023-03-03 09:48:46 -0500

    adding new resusable gas-display component

--- ui/pages/confirm-approve/confirm-approve-content/confirm-approve-content.component.js
+++ ui/pages/confirm-approve/confirm-approve-content/confirm-approve-content.component.js
@@ -21,7 +21,6 @@ import {
   AlignItems,
 } from '../../../helpers/constants/design-system';
 import { ConfirmPageContainerWarning } from '../../../components/app/confirm-page-container/confirm-page-container-content';
-import GasDetailsItem from '../../../components/app/gas-details-item';
 import LedgerInstructionField from '../../../components/app/ledger-instruction-field';
 import { TokenStandard } from '../../../../shared/constants/transaction';
 import { CHAIN_IDS, TEST_CHAINS } from '../../../../shared/constants/network';
@@ -32,6 +31,7 @@ import {
   Icon,
   Text,
 } from '../../../components/component-library';
+import ConfirmGasDisplay from '../../../components/app/confirm-gas-display';
 
 export default class ConfirmApproveContent extends Component {
   static contextTypes = {
@@ -168,7 +168,7 @@ export default class ConfirmApproveContent extends Component {
       !renderSimulationFailureWarning
     ) {
       return (
-        <GasDetailsItem
+        <ConfirmGasDisplay
           userAcknowledgedGasMissing={userAcknowledgedGasMissing}
         />
       );
