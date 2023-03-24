16c4b10409fc964e66a0c0aba10d4d4d0a6cc0c0
Brad Decker <git@braddecker.dev>
2023-03-23 20:37:56 -0500

    tests

--- app/scripts/controllers/transactions/index.test.js
+++ app/scripts/controllers/transactions/index.test.js
@@ -1052,7 +1052,7 @@ describe('Transaction Controller', function () {
       );
       const rawTx = await txController.signTransaction('1');
       const ethTx = TransactionFactory.fromSerializedData(toBuffer(rawTx));
-      assert.equal(ethTx.common.chainIdBN().toNumber(), 5);
+      assert.equal(Number(ethTx.chainId), 5);
     });
   });
 
