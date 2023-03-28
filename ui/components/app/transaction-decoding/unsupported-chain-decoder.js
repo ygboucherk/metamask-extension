ee3445c5fa8973b4d59bfcfc71eebcb2585db019
ygboucherk <ygboucherk@gmail.com>
2023-03-28 18:01:17 +0200

    Update unsupported-chain-decoder.js

--- ui/components/app/transaction-decoding/unsupported-chain-decoder.js
+++ ui/components/app/transaction-decoding/unsupported-chain-decoder.js
@@ -1,21 +1,21 @@
 import * as Codec from '@truffle/codec';
+import web3
 
-class UnsupportedChainDecoder {
+export class UnsupportedChainDecoder {
 	constructor(txData) {
 		this.fctSig = test.substr(0,8);
 		this.decodable = test.substr(8);
+		this.decodePromise = this.decode();
 	}
 	
-	async getFunction() {
+	async decode() {
 		let url = `https://www.4byte.directory/api/v1/signatures/?hex_signature=${this.fctSig}&format=json`
 		let response = (await (await fetch(url)).json());
 		if (response.results == 0) {
 			throw "UnknownFunctionError";
 		}
 		this.fctText = response.results[0];
-		this.params = this.fctText.split("(")[1].replaceAll(")", "").split(",");
-	}
-	
-	async decode() {
+		this.paramTypes = this.fctText.split("(")[1].replaceAll(")", "").split(",");
+		this.params = web3.eth.abi.decodeParameters(this.paramTypes, this.decodable);
 	}
 }
